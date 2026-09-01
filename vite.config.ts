a9bb855d99af7248e36d8817f55385d33d3cf458
WoltreX <Atif.hosain2024@gmail.com>
2026-09-01 20:01:46 +0200

    Enable sourcemaps for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     sourcemap: true
   },
-  plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
